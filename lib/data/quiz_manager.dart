0bd3cc73f50d9824eb76d0b615a7b158188d4da3
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-08-03 02:36:51 -0700

    impl QuizManager class

---
+++ lib/data/quiz_manager.dart
@@ -0,0 +1,20 @@
+import 'quiz_model.dart';
+
+class QuizManager{
+  QuizManager._();
+
+  List<QuizModel> getAllQuiz(){
+    final QuizModel quiz1 = QuizModel(
+      questionTitle: "نام سرادار ایرانی که موفق شد برای اولین بار رومی ها را شکست دهد؟",
+      answerList: ["سورنا","بابک خرمدین","شاپور اول"],
+      correctAnswer: 2
+    );
+    final QuizModel quiz2 = QuizModel(
+        questionTitle: "کدام امپراطور روم خدمتکار مخصوص پادشاه ایران شد؟",
+        answerList: ["مارک انتونی","کراسوس","والرین"],
+        correctAnswer: 0
+    );
+    final List<QuizModel> data = [quiz1,quiz2];
+    return data;
+  }
+}
--- lib/data/quiz_model.dart
+++ lib/data/quiz_model.dart
@@ -2,4 +2,6 @@ class QuizModel{
   String? questionTitle;
   int? correctAnswer;
   List<String>? answerList;
+
+  QuizModel({this.questionTitle, this.correctAnswer, this.answerList});
 }
